f693663097e8e804a477201380a51a981b1eb988
Kasi <93147592+callmekasi@users.noreply.github.com>
2023-12-22 14:08:02 +0000

    Add files via upload

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,6 +9,25 @@ async function fetchAcronymsList() {
     }
 }
 
+async function translateAndDisplay() {
+    const inputText = document.getElementById('translateMessage').value;
+    const acronymsList = await fetchAcronymsList();
+
+    const translationMap = {};
+    acronymsList.split('\n').forEach(line => {
+        const [acronym, meaning] = line.split('=');
+        if (acronym && meaning) {
+            translationMap[acronym.trim().toLowerCase()] = meaning.trim();
+        }
+    });
+
+    const words = inputText.match(/\w+|[^\w\s]/g);
+    const translatedWords = words.map(word => translationMap[word.toLowerCase()] || word);
+
+    const translatedPhrase = translatedWords.join(' ');
+    document.getElementById('translatedResult').innerText = translatedPhrase;
+}
+
 async function translateAcronyms(inputString) {
     const acronymsList = await fetchAcronymsList();
 
